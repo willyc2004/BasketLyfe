92ffe5730fa619cf2cb6f7a022a9e6360b382fc9
willyc2004 <willy.cahyadi25@gmail.com>
2024-01-09 16:59:50 +0700

    FormSchedule push

--- app/src/main/java/com/example/basketlyfe/ui/view/FormSchedule.kt
+++ app/src/main/java/com/example/basketlyfe/ui/view/FormSchedule.kt
@@ -11,9 +11,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,11 +28,58 @@ import com.example.basketlyfe.components.TextBoldMod
 import com.example.basketlyfe.components.TextExtraBold
 import com.example.basketlyfe.components.TextField
 import com.example.basketlyfe.viewmodel.FormScheduleViewModel
+import com.maxkeppeker.sheets.core.models.base.rememberUseCaseState
+import com.maxkeppeler.sheets.date_time.DateTimeDialog
+import com.maxkeppeler.sheets.date_time.models.DateTimeSelection
+import com.maxkeppeler.sheets.option.OptionDialog
+import com.maxkeppeler.sheets.option.models.DisplayMode
+import com.maxkeppeler.sheets.option.models.Option
+import com.maxkeppeler.sheets.option.models.OptionConfig
+import com.maxkeppeler.sheets.option.models.OptionSelection
+import java.time.LocalTime
 
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun FormSchedule(viewModel: FormScheduleViewModel) {
+    val selectedTime = remember { mutableStateOf<LocalTime?>(LocalTime.now()) }
+    val clockState = rememberUseCaseState()
+    val optionState = rememberUseCaseState()
+    DateTimeDialog(
+        state = clockState,
+        selection = DateTimeSelection.Time (
+            selectedTime = selectedTime.value
+        ) {newTime ->
+            selectedTime.value = newTime
+        }
+    )
+
+    val options = listOf(
+        Option(
+            titleText = "2 Jam",
+            selected = true
+        ),
+        Option(
+            titleText = "4 Jam",
+//            disabled = true
+        ),
+        Option(
+            titleText = "6 Jam",
+//            details = OptionDetails(
+//                "Ananas comosus",
+//                "The pineapple is a tropical plant with an edible fruit; it is the most economically significant plant in the family Bromeliaceae."
+//            )
+        ),
+
+        )
+
+    OptionDialog(
+        state = optionState,
+        selection = OptionSelection.Single(options) { index, option ->
+            // Handle selection
+        },
+        config = OptionConfig(mode = DisplayMode.LIST)
+    )
     Surface(
         color = Color.White,
         modifier = Modifier
@@ -56,6 +107,18 @@ fun FormSchedule(viewModel: FormScheduleViewModel) {
             TextField(value = viewModel.maxPlayer, onValueChange = {viewModel.maxPlayer = it}, placeHolder = "Max Player")
 
             Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {
+                clockState.show()
+            }) {
+                Text(text = "Time Start")
+            }
+
+            Button(onClick = {
+                optionState.show()
+            }) {
+                Text(text = "Berapa Jam?")
+            }
         }
     }
 }
