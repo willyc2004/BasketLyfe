2f0096b6d0fdd34f16dd9a8f3cb88fd04b7469f0
Gonlsali <radhita09@gmail.com>
2024-01-09 22:06:18 +0700

    Login

--- app/src/main/java/com/example/basketlyfe/viewmodel/MasukViewModel.kt
+++ app/src/main/java/com/example/basketlyfe/viewmodel/MasukViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.basketlyfe.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
+import com.example.basketlyfe.data.DataStoreManager
+import com.example.basketlyfe.repositories.MyDBContainer
 import com.example.basketlyfe.ui.ListScreen
 import kotlinx.coroutines.launch
 
@@ -22,13 +25,51 @@ class MasukViewModel (private val navController: NavController): ViewModel() {
         password = newPassword
     }
 
-    fun onLoginClicked() {
+    fun onLoginClicked(
+        email: String,
+        password: String,
+        dataStore: DataStoreManager,
+        navController: NavController
+    ) {
         // Implement your login logic here
         // For example, you can use viewModelScope.launch for background tasks
         viewModelScope.launch {
-            navController.navigate(ListScreen.MainScreen.name)
+            try {
+                val response = MyDBContainer().myDBRepositories.login(email, password)
+
+                if (response.message.equals("User Not Found", true)) {
+                    navController.navigate(ListScreen.Masuk.name)
+                } else if (response.message.equals("Incorrect Password", true)) {
+                    navController.navigate(ListScreen.Masuk.name)
+                } else {
+                    val data = response.data as List<*>
+                    Log.d("Login response", response.data.toString())
+
+                    dataStore.saveToken(data[1] as String)
+                    dataStore.saveEmail(data[0] as String)
+
+                    navController.navigate(ListScreen.MainScreen.name)
+
+                    dataStore.getToken.collect{
+                        if (it != null) {
+                            MyDBContainer.EMAIL = it
+                        }
+                    }
+
+                    dataStore.getToken.collect{
+                        if (it != null) {
+                            MyDBContainer.ACCESS_TOKEN = it
+                        }
+                    }
+                }
+
+            } catch (e: Exception) {
+                Log.d("Login Error", e.message.toString())
+                navController.navigate(ListScreen.Masuk.name)
+            }
         }
     }
+
     fun onDaftarClicked() {
         // Handle any logic related to the Daftar button click
         navController.navigate(ListScreen.Daftar.name)
